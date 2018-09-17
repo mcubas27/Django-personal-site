6c9cce79540191a441d2d0c73aaeff4593de10b3
mcubas27 <42919670+mcubas27@users.noreply.github.com>
2018-09-16 21:51:24 -0400

    Added EB database config and requirements files

--- mcubas_site/settings.py
+++ mcubas_site/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'www','static')
 
 
 EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
